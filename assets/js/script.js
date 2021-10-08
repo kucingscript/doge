ab711653fcff773dfd83e710f45e1ce958d3cebc
rasyidzkun <rasyidzkun@gmail.com>
2021-10-08 20:28:05 +0700

    menambahkan tema baru

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,30 @@ new fullpage("#fullpage", {
   scrollingSpeed: 1000,
   navigationTooltips: ["Home"],
 });
+
+const themeDots = document.getElementsByClassName("theme-dot");
+for (let i = 0; i < themeDots.length; i++) {
+  themeDots[i].addEventListener("click", function () {
+    const mode = this.dataset.mode;
+    setTheme(mode)
+  })
+}
+
+function setTheme(mode) {
+  if (mode === "dark-blue") {
+    document.getElementById("theme-style").href = "assets/css/style.css"
+  }
+
+  if (mode === "dark-red") {
+    document.getElementById("theme-style").href = "assets/css/theme/dark-red.css"
+  }
+  if (mode === "green") {
+    document.getElementById("theme-style").href = "assets/css/theme/green.css"
+  }
+  if (mode === "soft-red") {
+    document.getElementById("theme-style").href = "assets/css/theme/soft-red.css"
+  }
+  if (mode === "violet") {
+    document.getElementById("theme-style").href = "assets/css/theme/violet.css"
+  }
+}
